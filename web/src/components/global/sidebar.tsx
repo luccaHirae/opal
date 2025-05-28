daf8791ad12f57a1eb17d101d572f482e94f606f
Bruno Hirae <brunodeluca117@outlook.com>
2025-05-28 15:37:12 -0300

    feat: enhance sidebar with workspace invitation modal and user notifications

--- web/src/components/global/sidebar.tsx
+++ web/src/components/global/sidebar.tsx
@@ -9,11 +9,18 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { Separator } from '@/components/ui/separator';
 import { QUERY_KEYS } from '@/lib/react-query';
 import { getUserWorkspaces } from '@/actions/workspace';
 import { useQuery } from '@tanstack/react-query';
+import { Modal } from '@/components/global/modal';
+import { PlusCircleIcon } from 'lucide-react';
+import { UsersSearch } from '@/components/global/user-search';
+import { MENU_ITEMS } from '@/constants';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { getUserNotifications } from '@/actions/user';
 
 interface SidebarProps {
   activeWorkSpaceId: string;
@@ -21,15 +28,29 @@ interface SidebarProps {
 
 export function Sidebar({ activeWorkSpaceId }: SidebarProps) {
   const router = useRouter();
+  const pathname = usePathname();
+
   const { data } = useQuery({
     queryKey: [QUERY_KEYS.USER_WORKSPACES],
     queryFn: getUserWorkspaces,
     enabled: !!activeWorkSpaceId,
   });
 
+  const { data: notificationsData } = useQuery({
+    queryKey: [QUERY_KEYS.USER_NOTIFICATIONS],
+    queryFn: getUserNotifications,
+    enabled: !!activeWorkSpaceId,
+  });
+
   const onChangeActiveWorkspace = (value: string) => {
     router.push(`/dashboard/${value}`);
   };
+
+  const currentWorkSpace = data?.workSpaces?.find(
+    (workSpace) => workSpace.id === activeWorkSpaceId
+  );
+
+  const menuItems = MENU_ITEMS(activeWorkSpaceId);
 
   return (
     <div className='bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col gap-4 items-center overflow-hidden'>
@@ -69,6 +90,162 @@ export function Sidebar({ activeWorkSpaceId }: SidebarProps) {
             )}
         </SelectContent>
       </Select>
+
+      {currentWorkSpace?.type === 'PUBLIC' &&
+        data?.subscription?.plan === 'PRO' && (
+          <Modal
+            title='Invite To Workspace'
+            description='Invite other users to your workspace'
+            trigger={
+              <span className='text-sm cursor-pointer flex items-center justify-center bg-neutral-800/90 hover:bg-neutral-800/60 w-full rounded-sm p-[5px] gap-2'>
+                <PlusCircleIcon
+                  size={15}
+                  className='text-neutral-800/90 fill-neutral-500'
+                />
+                <span className='text-neutral-400 font-semibold text-xs'>
+                  Invite To Workspace
+                </span>
+              </span>
+            }
+          >
+            <UsersSearch workspaceId={activeWorkSpaceId} />
+          </Modal>
+        )}
+
+      <p className='w-full text-[#9d9d9d] font-bold mt-4'>Menu</p>
+
+      <nav className='w-full'>
+        <ul>
+          {menuItems.map((item) => (
+            <SidebarItem
+              href={item.href}
+              icon={item.icon}
+              selected={pathname === item.href}
+              title={item.title}
+              key={item.title}
+              notifications={
+                (item.title === 'Notifications' && notificationsData?.count) ||
+                0
+              }
+            />
+          ))}
+        </ul>
+      </nav>
+
+      <Separator className='w-4/5' />
+
+      <p className='w-full text-[#9d9d9d] font-bold mt-4'>Workspaces</p>
+
+      {data?.workSpaces?.length === 1 && data.members?.length === 0 && (
+        <div className='w-full mt-[-10px]'>
+          <p className='text-[#3c3c3c] font-medium text-sm'>
+            {data?.subscription?.plan === 'FREE'
+              ? 'Upgrade to create workspaces'
+              : 'No workspaces.'}
+          </p>
+        </div>
+      )}
+
+      <nav className='w-full'>
+        <ul className='h-[150px] overflow-auto overflow-x-hidden fade-layer'>
+          {data?.workSpaces &&
+            data?.workSpaces?.length > 0 &&
+            data?.workSpaces?.map(
+              (workSpace) =>
+                workSpace.type !== 'PERSONAL' && (
+                  <SidebarItem
+                    href={`/dashboard/${workSpace.id}`}
+                    selected={pathname === `/dashboard/${workSpace.id}`}
+                    title={workSpace.name}
+                    notifications={0}
+                    key={workSpace.name}
+                    icon={
+                      <WorkspacePlaceholder>
+                        {workSpace.name.charAt(0)}
+                      </WorkspacePlaceholder>
+                    }
+                  />
+                )
+            )}
+
+          {data?.members &&
+            data.members?.length > 0 &&
+            data.members.map((member) => (
+              <SidebarItem
+                href={`/dashboard/${member.workSpace.id}`}
+                selected={pathname === `/dashboard/${member.workSpace.id}`}
+                title={member.workSpace.name}
+                notifications={0}
+                key={member.workSpace.name}
+                icon={
+                  <WorkspacePlaceholder>
+                    {member.workSpace.name.charAt(0)}
+                  </WorkspacePlaceholder>
+                }
+              />
+            ))}
+        </ul>
+      </nav>
+
+      <Separator className='w-4/5' />
+
+      {
+        data?.subscription?.plan === 'FREE' && (
+          <></>
+        ) /* Placeholder for future features */
+      }
     </div>
+  );
+}
+
+interface SidebarItemProps {
+  title: string;
+  href: string;
+  icon: React.ReactNode;
+  selected: boolean;
+  notifications?: number;
+}
+
+export function SidebarItem({
+  title,
+  href,
+  icon,
+  selected,
+}: // notifications = 0,
+SidebarItemProps) {
+  return (
+    <li className='cursor-pointer my-[5px]'>
+      <Link
+        href={href}
+        className={cn(
+          'flex items-center justify-between group rounded-lg hover:bg-[#1d1d1d]',
+          selected ? 'bg-[#1d1d1d]' : ''
+        )}
+      >
+        <div className='flex items-center gap-2 transition-all p-[5px] cursor-pointer'>
+          {icon}
+          <span
+            className={cn(
+              'font-medium group-hover:text-[#9d9d9d] transition-all truncate w-32',
+              selected ? 'text-[#9d9d9d]' : 'text-[#545454]'
+            )}
+          >
+            {title}
+          </span>
+        </div>
+      </Link>
+    </li>
+  );
+}
+
+export function WorkspacePlaceholder({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <span className='bg-[#545454] flex items-center font-bold justify-center w-8 px-2 h-7 rounded-sm text-[#1d1d1d]'>
+      {children}
+    </span>
   );
 }
--- web/src/lib/react-query.ts
+++ web/src/lib/react-query.ts
@@ -1,11 +1,15 @@
 import { QueryClient } from '@tanstack/react-query';
 
-// Query Keys
 export const QUERY_KEYS = {
   WORKSPACE_FOLDERS: 'workspace-folders',
   USER_VIDEOS: 'user-videos',
   USER_WORKSPACES: 'user-workspaces',
   USER_NOTIFICATIONS: 'user-notifications',
+  GET_USERS: 'get-users',
+};
+
+export const MUTATION_KEYS = {
+  INVITE_MEMBER: 'invite-member',
 };
 
 export const queryClient = new QueryClient();
