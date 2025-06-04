338d43a3a34ed8b92e21e412eb025f05f5923d87
Bruno Hirae <brunodeluca117@outlook.com>
2025-06-04 14:56:27 -0300

    feat: implement getFolderDetails function to retrieve folder information

--- web/src/actions/workspace.ts
+++ web/src/actions/workspace.ts
@@ -319,3 +319,45 @@ export const createFolder = async (workspaceId: string) => {
     };
   }
 };
+
+export const getFolderDetails = async (folderId: string) => {
+  try {
+    const folder = await client.folder.findUnique({
+      where: {
+        id: folderId,
+      },
+      select: {
+        name: true,
+        _count: {
+          select: {
+            videos: true,
+          },
+        },
+      },
+    });
+
+    if (folder) {
+      return {
+        status: 200,
+        folder,
+      };
+    }
+
+    return {
+      status: 404,
+      message: 'Folder not found',
+    };
+  } catch (err) {
+    if (err instanceof Error) {
+      return {
+        status: 500,
+        message: err.message,
+      };
+    }
+
+    return {
+      status: 500,
+      message: 'An unexpected error occurred',
+    };
+  }
+};
